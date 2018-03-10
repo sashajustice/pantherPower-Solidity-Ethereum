af3f616bb6617d1ccaa5dbe152ef32735b573042
sashajustice <sabrinnasreddin@gmail.com>
2018-03-09 17:18:42 -0800

    add address, tot supply, balancesecond part

--- pantherPower.sol
+++ pantherPower.sol
@@ -7,4 +7,31 @@ contract PantherPower is IERC20 {
   string public constant symbol = "Power";
   string public constant name = "PantherPower";
   uint public constant decimals = 3;
+
+mapping(address => uint256) balances;
+mapping(address => mapping(address => uint256)) allowed;
+
+function PantherPower() {
+balances[msg.sender] = _totalSupply;
+}
+
+function totalSupply() constant returns (uint totalSupply) {
+return _totalSupply;>>>>
+}
+
+function balanceOf(address _owner) constant returns (uint256 balance) {
+return balances[_owner];
+}
+
+function transfer(address _to, uint256 _value) returns (bool success) {
+require(
+    balances[msg.sender] >= _value
+    && _value > 0
+);
+balances[msg.sender] -= _value;
+balances[_to] += _value;
+Transfer(msg.sender, _to, _value);
+return true;
+}
+
 }
