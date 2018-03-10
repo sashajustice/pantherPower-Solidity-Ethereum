c09fa83276c4b1971ec0c172b5c01cf35162edb6
sashajustice <sabrinnasreddin@gmail.com>
2018-03-09 19:19:59 -0800

    finishe contract

--- pantherPower.sol
+++ pantherPower.sol
@@ -34,4 +34,35 @@ Transfer(msg.sender, _to, _value);
 return true;
 }
 
+function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+require(
+    allowed[_from][msg.sender] >= _value
+    && balances[_from] >= _value
+    && _value > 0
+);
+balances[_from] -= _value;
+balances[_to] += _value;
+allowed[_from][msg.sender] -= _value;
+Transfer(_from, _to, _value);
+return true;
+}
+
+function approve(address _spender, uint256 _value) returns (bool success) {
+allowed[msg.sender][_spender] = _value;
+Approval(msg.sender, _spender, _value);
+return true;
+}
+
+function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+return allowed[_owner][_spender];
+}
+
+function() payable {
+ revert();
+
+}
+event Transfer(address indexed _from, address indexed _to, uint256 _value);
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}    
+
 }
